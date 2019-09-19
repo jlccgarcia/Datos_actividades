0c24731e7f1ac5e1261993f7ba1ec74ced388ccb
jlccgarcia <55311434+jlccgarcia@users.noreply.github.com>
2019-09-19 17:33:46 +0200

    Tema 3 - paso datos entre actividades

---
+++ app/src/main/java/com/example/datos_actividades/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.datos_actividades;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void aSumar(View view){
+        EditText cuadro1=(EditText)findViewById(R.id.num1);
+        EditText cuadro2=(EditText)findViewById(R.id.num2);
+
+        int n1=Integer.parseInt(cuadro1.getText().toString());
+        int n2=Integer.parseInt(cuadro2.getText().toString());
+
+        Intent i=new Intent(this, sumar.class);
+
+        i.putExtra("numero1", n1);
+        i.putExtra("numero2", n2);
+
+        startActivity(i);
+    }
+}
